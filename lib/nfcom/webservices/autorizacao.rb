bf739cd8ec0da0d692f9dec43ab003f830fe049e
Keith Yoder <yoder@tessi.com.br>
2026-01-16 09:49:32 -0300

    Xml autorizado correcao (#4)
    * corrigir a geração do xml_assinado

--- lib/nfcom/webservices/autorizacao.rb
+++ lib/nfcom/webservices/autorizacao.rb
@@ -5,13 +5,12 @@ module Nfcom
     # Serviço de autorização de NFCom
     #
     # Responsável por enviar NFCom para autorização na SEFAZ,
-    # incluindo compressão GZIP, envio via SOAP 1.2 e processamento
-    # da resposta.
+    # incluindo compressão GZIP e envio via SOAP 1.2.
     class Autorizacao < Base
       # Envia NFCom assinada para autorização
       #
       # @param xml_assinado [String] XML da NFCom já assinado
-      # @return [Hash] Hash com resultado da autorização
+      # @return [Nokogiri::XML::Document] XML bruto da resposta do SEFAZ
       # @raise [Errors::ConfigurationError] Se URL não configurada
       # @raise [Errors::SefazError] Se houver erro na comunicação
       def enviar(xml_assinado)
@@ -32,10 +31,9 @@ module Nfcom
 
         # Enviar para SEFAZ
         action = 'http://www.portalfiscal.inf.br/nfcom/wsdl/NFComRecepcao/nfcomRecepcao'
-        response = post_soap_with_compression(url: url, action: action, xml: soap_request)
+        post_soap_with_compression(url: url, action: action, xml: soap_request)
 
-        # Processar resposta
-        parse_response(response)
+        # Retorna apenas o XML como Nokogiri::XML::Document
       rescue StandardError => e
         configuration.logger&.error("Erro ao enviar NFCom: #{e.message}")
         raise
@@ -44,16 +42,12 @@ module Nfcom
       private
 
       # Constrói envelope SOAP 1.2 para NFCom
-      #
-      # IMPORTANTE: O namespace vai NO nfcomDadosMsg, NÃO no envelope!
-      # E o nfcomDadosMsg vai DIRETO no Body, sem wrapper!
       def build_soap_envelope(xml_comprimido)
         require 'nokogiri'
 
         builder = Nokogiri::XML::Builder.new do |xml|
           xml['soap'].Envelope('xmlns:soap' => 'http://www.w3.org/2003/05/soap-envelope') do
             xml['soap'].Body do
-              # IMPORTANTE: Namespace vai AQUI e sem wrapper!
               xml.nfcomDadosMsg(
                 xml_comprimido,
                 'xmlns' => 'http://www.portalfiscal.inf.br/nfcom/wsdl/NFComRecepcao'
@@ -62,11 +56,9 @@ module Nfcom
           end
         end
 
-        # Gerar XML sem formatação (save_with: 0 = sem pretty print)
         builder.to_xml(save_with: Nokogiri::XML::Node::SaveOptions::AS_XML)
       end
 
-      # Envia requisição SOAP (override de Base para adicionar certificado)
       def post_soap_with_compression(url:, action:, xml:)
         http, uri = build_http_client(url)
         request = build_soap_request(uri, action, xml)
@@ -85,14 +77,7 @@ module Nfcom
         raise Errors::SefazError, "Erro SSL: #{e.message}"
       end
 
-      def parse_response(soap_response)
-        ret_doc = decompress_response(soap_response)
-
-        log_decompressed_xml(ret_doc)
-
-        extract_nfcom_result(ret_doc)
-      end
-
+      # HTTP client + certificado
       def build_http_client(url)
         uri = URI.parse(url)
         http = Net::HTTP.new(uri.host, uri.port)
@@ -139,61 +124,6 @@ module Nfcom
 
         body = response.body.force_encoding('UTF-8')
         configuration.logger&.debug("SOAP Response (raw):\n#{body}")
-      end
-
-      def decompress_response(soap_response)
-        doc = Utils::ResponseDecompressor.extract_and_decompress(soap_response)
-        doc.remove_namespaces!
-        doc
-      end
-
-      def extract_nfcom_result(doc)
-        ret = doc.at_xpath('//retNFCom')
-        return log_missing_ret_nfcom(doc) unless ret
-
-        build_result(ret)
-      end
-
-      def build_result(ret)
-        result = {
-          c_stat: ret.at_xpath('.//cStat')&.text,
-          x_motivo: ret.at_xpath('.//xMotivo')&.text
-        }
-
-        if (prot = ret.at_xpath('.//protNFCom'))
-          result[:prot_nfcom] = extract_protocol(prot)
-        end
-
-        log_extracted_result(result)
-        result
-      end
-
-      def extract_protocol(prot)
-        {
-          n_prot: prot.at_xpath('.//nProt')&.text,
-          ch_nfcom: prot.at_xpath('.//chNFCom')&.text,
-          dh_rec_bto: prot.at_xpath('.//dhRecbto')&.text,
-          xml: prot.to_xml
-        }
-      end
-
-      def log_missing_ret_nfcom(doc)
-        configuration.logger&.warn(
-          "Resposta não contém retNFCom. XML: #{doc.to_xml}"
-        )
-        {}
-      end
-
-      def log_decompressed_xml(doc)
-        return unless configuration.log_level == :debug
-
-        configuration.logger&.debug("XML da Resposta:\n#{doc.to_xml}")
-      end
-
-      def log_extracted_result(result)
-        return unless configuration.log_level == :debug
-
-        configuration.logger&.debug("Resultado extraído: #{result.inspect}")
       end
     end
   end
